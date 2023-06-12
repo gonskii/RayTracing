c39d13de892ea7e1ded928e298d64e521b8a9182
Bozeyy <spereaux2@gmail.com>
2023-06-12 14:47:25 +0200

    creation du serviceCalcul

--- projet/ServiceDeCalcul/ServiceCalcul.java
+++ projet/ServiceDeCalcul/ServiceCalcul.java
@@ -1,5 +1,25 @@
 package projet.ServiceDeCalcul;
 
+import projet.raytracer.Image;
+
 public class ServiceCalcul {
+
+
+
+    /**
+     * méthode calculerBoutScene
+     * @param Scene
+     * @param x
+     * @param y
+     * @param largeur
+     * @param hauteur
+     */
+    public Image calculerBoutScene(Scene Scene, int x, int y, int largeur, int hauteur) {
+        // TODO implement here
+
+
+        return new Image(largeur, hauteur);
+    }
+
     
 }
