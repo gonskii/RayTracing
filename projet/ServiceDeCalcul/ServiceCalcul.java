911a2ed5b376629c4eb5cb24edfb7fda8b67451c
GaspardBBY <96123850+GaspardBBY@users.noreply.github.com>
2023-06-13 13:21:14 +0200

    feat: added for client timer

--- projet/ServiceDeCalcul/ServiceCalcul.java
+++ projet/ServiceDeCalcul/ServiceCalcul.java
@@ -4,6 +4,8 @@ import raytracer.Image;
 import raytracer.Scene;
 
 import java.io.Serializable;
+import java.time.Duration;
+import java.time.Instant;
 
 public class ServiceCalcul implements InterfaceServiceCalcul, Serializable {
 
@@ -11,16 +13,22 @@ public class ServiceCalcul implements InterfaceServiceCalcul, Serializable {
      * méthode calculerBoutScene
      *
      * @param scene   la scène à calculer
-     * @param x       coordonnée x du coin supérieur gauche de la partie de la scène à calculer
-     * @param y       coordonnée y du coin supérieur gauche de la partie de la scène à calculer
+     * @param x       coordonnée x du coin supérieur gauche de la partie de la scène
+     *                à calculer
+     * @param y       coordonnée y du coin supérieur gauche de la partie de la scène
+     *                à calculer
      * @param largeur largeur de la partie de la scène à calculer
      * @param hauteur hauteur de la partie de la scène à calculer
      */
     @Override
     public Image calculerBoutScene(Scene scene, int x, int y, int largeur, int hauteur) {
-        System.out.println("Calcul de la partie de la scène : " + x + " " + y + " " + largeur + " " + hauteur);
+        System.out.println("Calcul de la partie de la scene : " + x + " " + y + " " + largeur + " " + hauteur);
+        Instant debut = Instant.now();
         Image image = scene.compute(x, y, largeur, hauteur);
-        System.out.println("Calcul terminé");
+        Instant fin = Instant.now();
+        long duree = Duration.between(debut, fin).toMillis();
+
+        System.out.println("Calcul termine en:" + duree + " ms");
         return image;
     }
 }
