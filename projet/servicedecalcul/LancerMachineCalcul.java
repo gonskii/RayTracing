50a6cca120d66377328508d71024a603daf89c2b
GaspardBBY <96123850+GaspardBBY@users.noreply.github.com>
2023-06-16 12:44:37 +0200

    fix: added scanner & args

--- projet/servicedecalcul/LancerMachineCalcul.java
+++ projet/servicedecalcul/LancerMachineCalcul.java
@@ -6,23 +6,26 @@ import java.util.Scanner;
 
 public class LancerMachineCalcul {
     public static void main(String[] args) throws Exception {
+        String ip;
+        int port;
         if (args.length != 2) {
-            System.out.println("Usage: java LancerMachineCalcul <ip du serveur> <port du serveur>");
-            System.exit(0);
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Entrez l'ip du serveur");
+            ip = sc.nextLine();
+            System.out.println("Entrez le port du serveur");
+            port = sc.nextInt();
+            sc.close();
+        } else {
+            ip = args[0];
+            port = Integer.parseInt(args[1]);
         }
-        String ip = args[0];
-        int port = Integer.parseInt(args[1]);
-        //Utilisation des scanners:
-        Scanner sc = new Scanner(System.in); 
-        String ip = sc.nextLine();
-        int port = sc.nextInt();
-        sc.close();
 
         Registry reg = LocateRegistry.getRegistry(ip, port);
         InterfaceServiceRaytracing serviceRaytracing = (InterfaceServiceRaytracing) reg.lookup("ServiceRaytracing");
-        
+
         ServiceCalcul servicecalcul = new ServiceCalcul();
-        InterfaceServiceCalcul serviceCalculRemote = (InterfaceServiceCalcul) UnicastRemoteObject.exportObject(servicecalcul, 0);
+        InterfaceServiceCalcul serviceCalculRemote = (InterfaceServiceCalcul) UnicastRemoteObject
+                .exportObject(servicecalcul, 0);
         serviceRaytracing.enregistrerMachineQuiCalcul(serviceCalculRemote);
         System.out.println("ServiceCalcul enregistré auprès du serveur");
     }
